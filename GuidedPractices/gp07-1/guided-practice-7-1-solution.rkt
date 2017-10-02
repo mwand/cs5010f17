177b86dde376149211d6f2b271e7155a85c24d7a
Mitchell Wand <wand@ccs.neu.edu>
2017-10-02 13:50:40 -0400

    module07 ready for release

--- GuidedPractices/gp07-1/guided-practice-7-1-solution.rkt
+++ GuidedPractices/gp07-1/guided-practice-7-1-solution.rkt
@@ -2,7 +2,7 @@
 
 ;; design a function
 
-;; diff : NonEmptyListOfNumber -> Number
+;; diff : NonEmptyNumberList -> Number
 ;; GIVEN: a nonempty list of numbers
 ;; RETURNS: the result of subtracting the numbers, from left to right.
 ;; EXAMPLE:
@@ -12,13 +12,13 @@
 
 ;; Use the data definition
 
-;; NonEmptyListOfNumber = (cons Number ListOfNumber)
+;; NonEmptyNumberList = (cons Number NumberList)
 
 ;; You will need one function for the whole list, and one for the
-;; ListOfNumber.
+;; NumberList.
 
-;; Your function for the ListOfNumber must follow the template for
-;; ListOfNumber, but you will need a context argument.  Think carefully
+;; Your function for the NumberList must follow the template for
+;; NumberList, but you will need a context argument.  Think carefully
 ;; about what the context argument means.
 
 
@@ -28,12 +28,12 @@
 
 ;; DATA DEFINITIONS:
 
-;; NonEmptyListOfNumber = (cons Number ListOfNumber)
+;; NonEmptyNumberList = (cons Number NumberList)
 ;; template:
 (define (nelst-fn nelst)
   (... (first nelst) (rest nelst)))
 
-;; ListOfNumber template:
+;; NumberList template:
 (define (lon-fn lon)
   (cond
     [(empty? lon) ...]
@@ -43,23 +43,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; diff : NonEmptyListOfNumber -> Number
+;; diff : NonEmptyNumberList -> Number
 ;; GIVEN: a nonempty list of numbers
 ;; RETURNS: the result of subtracting the numbers, from left to right.
 ;; EXAMPLE:
 ;; (diff (list 10 5 3)) = 2
-;; STRATEGY: Follow the template for NonEmptyListOfNumber
+;; STRATEGY: Follow the template for NonEmptyNumberList
 
 (define (diff nelst)
   (diff-inner (first nelst) (rest nelst)))
 
 
-;; diff-inner : Number ListOfNumber
+;; diff-inner : Number NumberList
 ;; GIVEN: a number num and a sublist lon of some list lon0
 ;; WHERE: num is the result of subtracting all the numbers in lon0
 ;; that are above lon from some number num0
 ;; RETURNS: the result of subtracting all the numbers in lon0 from num0
-;; STRATEGY: Follow the template for ListOfNumber
+;; STRATEGY: Follow the template for NumberList
 
 (define (diff-inner num lon)
   (cond
@@ -72,12 +72,12 @@
 ;; Now, we could do this one without an invariant, just by changing
 ;; the purpose statement:
 
-;; diff-inner : Number ListOfNumber -> Number
+;; diff-inner : Number NumberList -> Number
 ;; GIVEN: a number num and a list of numbers lon
 ;; RETURNS: the result of subtracting each of the numbers in lon from
 ;; num
 ;; EXAMPLE: (diff-inner 10 (list 5 3)) = 2
-;; STRATEGY: Follow the template for ListOfNumber
+;; STRATEGY: Follow the template for NumberList
 
 (define (diff-inner num lon)
   (cond
--- GuidedPractices/gp07-2/guided-practice-7-2.rkt
+++ GuidedPractices/gp07-2/guided-practice-7-2.rkt
@@ -1,31 +1,11 @@
-Consider the computation of
+;; example for guided practice 7-2
 
-(free-vars
-  (make-app
-    (make-lam
-      'x
-      (make-app
-        'x
-        (make-lam
-          'y
-          (make-app
-            'x
-            (make-app 'y 'z)))))
-    (make-lam
-      'y
-      (make-app
-        'u
-        (make-lam
-          'z
-          (make-app
-            'x
-            (make-app 'y 'z)))))))
+;; f : Integer -> Integer
+;; GIVEN/RETURNS: omitted
+;; INVARIANT: to be filled in
+(define (f n)
+  (cond
+    [(zero? n) 1]
+    [else (* 2 (f (- n 3)))]))
 
 
-Which of the following will appear as calls to free-vars-in-subexp during
-this computation?
-
-1.	(free-vars-in-subexp (list 'z 'y) (make-app 'y 'z))
-2.	(free-vars-in-subexp (list 'x 'y) (make-app 'y 'z))
-3.	(free-vars-in-subexp (list 'y 'x) (make-app 'y 'z))
-4.	(free-vars-in-subexp (list 'x 'z) (make-app 'y 'z))
