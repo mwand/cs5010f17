65106a9f2be0771e0836de19c326706e3d44f699
Mitchell Wand <wand@ccs.neu.edu>
2017-10-19 16:08:47 -0400

    working on L8.2 binary search

--- Examples/08-3-binary-search.java
+++ Examples/08-3-binary-search.java
@@ -1,105 +1,157 @@
 // to compile and run this file, say
-// javac 08-3-binary-search.java
-// java BinSearch
+// javac <thisfilename>.java
+// java -enableassertions BinSearch
+//
+// (Note: By default, assertions are disabled in Java.)
 
-// no objects here, all we have is a single class with some static methods
+// no objects here, all we have is a single class with some
 // fields and static methods
+
+// In Java, array A runs from 0 to A.length-1.
+
+// The following definition will make our purpose statements easier to read.
+
+// DEFINITION: A is _nondecreasing_ iff
+//            (0 <= i <= j <= A.length) implies A[i] <= A[j])
+ 
 
 class BinSearch {
 
-    static int binsearch_recursive (int[]A, int tgt) 
+    static int binsearch_recursive (int[]A, int tgt) {
 
-    // GIVEN: an array of ints A and a target 'tgt'
-    // WHERE: A is non-decreasing (ie, i <= j implies A[i] <= A[j])
-    // RETURNS: a number k such that 0 <= k <= N and f(k) = tgt if there is such a k,
-    //          otherwise -1
-    // STRATEGY: Call a more general function
+        // GIVEN: an array of ints A and a target 'tgt'
+        // WHERE: A is non-decreasing
+        // RETURNS: a number k such that
+        //         0 <= k < A.length
+        //     and f(k) = tgt
+        // if there is such a k,
+        //         otherwise returns -1
+        // STRATEGY: Call a more general function
 
-    {
-	return recursive_loop (0, A.length, A, tgt);
+        return recursive_loop (0, A.length, A, tgt);
     }
 
-    static int recursive_loop (int lo, int hi, int[]A, int tgt)
-	
-    // GIVEN: two integers lo and hi, an array of ints A, and a target
-    // tgt
-    // WHERE: A is non-decreasing (ie, i <= j implies A[i] <= A[j])
-    // RETURNS: a number k such that lo <= k <= hi and f(k) = tgt if
-    // there is such a k, otherwise -1.
+    static int recursive_loop (int lo, int hi, int[] A, int tgt) {
 
-    // HALTING MEASURE: max((hi-lo),0)
-    // JUSTIFICATION: (max (- hi lo) 0) is guaranteed non-negative,
-    // and it decreases at every recursive call because p is 
-    // eliminated.  Note that it is posible for lo to be greater than
-    // hi if the target is not present in the array. See slides for more detailed argument.
+        // GIVEN: two integers lo and hi, an array of ints A, and a target tgt
+        // WHERE: A is non-decreasing (ie, i <= j implies A[i] <= A[j])
+        //       0 <= lo <= hi <= A.length
+        // AND   (forall j)(0  <= j < lo       ==> A[j] < tgt)
+        // AND   (forall j)(hi <= j < A.length ==> A[j] > tgt)
+        
+        // RETURNS: a number k such that lo <= k < hi and f(k) = tgt if
+        //     there is such a k, otherwise -1.
 
-    {
-	if (lo > hi)
-	    // the search area is empty
-	    {return -1;};
-	int mid = (lo + hi) / 2;
-	 if (A[mid] == tgt)
-		// we have found the target
-	     {return mid;}
-	    if (A[mid] < tgt)
-		// the target can't be to the left of mid, so search right half
-		{return recursive_loop (mid+1, hi, A, tgt);}
-	    // otherwise the target can't be to the right of mid, so search left half
-	    {return recursive_loop (lo, mid-1, A, tgt);}
+        // HALTING MEASURE: max (hi-lo, 0)
+        // JUSTIFICATION: max (hi - lo, 0) is guaranteed non-negative,
+        // and it decreases at every recursive call because one of the
+        // following happens:
+        //     lo increases while hi stays the same
+        //     lo stays the same while hi decreases
+
+        if (lo == hi) {
+            // the search area is empty
+            return -1;
+        }
+        else { /* do nothing */}
+        // choose an element in [lo,hi) . 
+        int mid = (lo + hi) / 2;
+        // mid will be less than hi because quotient truncates down.
+        if (A[mid] == tgt) {
+            // we have found the target
+            return mid;
+        }
+        else if (A[mid] < tgt) {
+            // the target can't be to the left of mid, so search right half
+            return recursive_loop (mid+1, hi, A, tgt);
+        }
+        else {
+            // otherwise the target can't be to the right of mid, so
+            // search left half.
+            return recursive_loop (lo, mid, A, tgt);
+        }
     }
 
     
     static int binsearch_iterative (int[] A, int tgt) {
-    // GIVEN: An array A of integers and an integer target 'tgt'
-    // WHERE: A is sorted in ascending order (duplicates are possible)
-    // RETURNS: a non-negative value i such that A[i] = tgt or -1 if no such i exists
+        // GIVEN: An array A of integers and an integer target 'tgt'
+        // WHERE: A is sorted in ascending order (duplicates are possible)
+        // RETURNS: a non-negative value k such that A[k] = tgt
+        //     or -1 if no such k exists
 
-	int lo = 0;
-	int hi = A.length;
+        int lo = 0;        
+        int hi = A.length;
 
-	// INVARIANT:
-	// IF   there is some i in [0,A.length] such that A[i] = tgt
-	// THEN there is some i in [lo,hi] such that A[i] = tgt
+        // INVARIANT:
+        //       0 <= lo <= hi <= A.length
+        // AND   (forall j)(0  <= j < lo       ==> A[j] < tgt)
+        // AND   (forall j)(hi <= j < A.length ==> A[j] > tgt)
 
-	// HALTING MEASURE: max((hi - lo),0)
-	// JUSTIFICATION: Same as above.
-    
+        // Note that lo = 0 and hi = A.length makes the invariant
+        // true, since in both cases there is no such j.
 
-	while (lo <= hi) { 		// the search area is non-empty
-	    int mid = (lo + hi) / 2;
-	    if (A[mid] == tgt)
-		// we have found the target
-		{return mid;}
-	    if (A[mid] < tgt)
-		// the target can't be to the left of mid, so search right half
-		{lo = mid+1;};
-	    // otherwise the target can't be to the right of mid, so search left half
-	    {hi = mid - 1;}
-	};
+        // HALTING MEASURE: max((hi - lo),0)
+        // JUSTIFICATION: Same as above.
 
-	// the search area is empty
-	return -1;
+        while (lo < hi) {                 // the search area is non-empty
+            int mid = (lo + hi) / 2;
+            if (A[mid] == tgt) {
+                // we have found the target
+                return mid;
+            }
+            else if (A[mid] < tgt) {
+                // the target can't be to the left of mid, so search
+                // right half. 
+                lo = mid+1;
+            }
+            // otherwise the target can't be to the right of mid,
+            // so search left half. 
+            else
+                hi = mid;
+        }
+
+        // the search area is empty
+        return -1;
     }
 
-
     public static void main (String[] args) {
-	int[] A = {0,1,4,9,16,25,36,49};
-	int[] B = {0,1,2,3,3,8,9,10,10,12};
+        int[] A = {0,1,4,9,16,25,36,49};
+        int[] B = {0,1,2,3,3,8,9,10,10,12};
+        int[] C = {};
 
-	assert binsearch_recursive(A,9) == 3;
-	assert binsearch_recursive(A,10) == -1;
-	assert binsearch_recursive(A,-5) == -1;
-	assert binsearch_recursive(A,100) == -1;
-	assert (binsearch_recursive(B,3) == 3 || binsearch_recursive(B,3) == 4);
+        assert binsearch_recursive(A,9) == 3;
+        assert binsearch_recursive(A,10) == -1;
+        assert binsearch_recursive(A,-5) == -1;
+        assert binsearch_recursive(A,100) == -1;
+        assert (binsearch_recursive(B,3) == 3 || binsearch_recursive(B,3) == 4);
+        assert binsearch_recursive(C,17) == -1;
 
-	assert binsearch_iterative(A,9) == 3;
-	assert binsearch_iterative(A,10) == -1;
-	assert binsearch_iterative(A,-5) == -1;
-	assert binsearch_iterative(A,100) == -1;
-	assert (binsearch_iterative(B,3) == 3 || binsearch_iterative(B,3) == 4);
+        assert binsearch_iterative(A,9) == 3;
+        assert binsearch_iterative(A,10) == -1;
+        assert binsearch_iterative(A,-5) == -1;
+        assert binsearch_iterative(A,100) == -1;
+        assert (binsearch_iterative(B,3) == 3 || binsearch_iterative(B,3) == 4);
+        assert binsearch_iterative(C,17) == -1;
+
+        // try using PdpTestSuite
+        // timeout of 10 seconds
+        PdpTestSuite tests = new PdpTestSuite(10);
+
+               tests.addTestCase("recursive (A,9) => 3",
+                          () -> binsearch_recursive(A,9) == 3,
+                          true);
+
+               // should have all the other tests here as well...
+
+               tests.addTestCase("1 = 2 (should fail)",
+                          () -> 1 == 2,
+                          true);
+
+        tests.addTestCase("3 = 3",
+                          () -> 3 == 3,
+                          true);
+
+        tests.runTests();
 
     }
 }
-	    
-	    
-	
