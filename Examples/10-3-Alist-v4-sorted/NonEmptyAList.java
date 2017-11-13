ad62f37311d99ceaea6a5d44e8bff7ca8e0587f6
Mitchell Wand <wand@ccs.neu.edu>
2017-11-13 18:48:46 -0500

    added examples for lecture 10

---
+++ Examples/10-3-Alist-v4-sorted/NonEmptyAList.java
@@ -0,0 +1,55 @@
+// The object constructed by NonEmptyAlst(k,v,rest) represents an
+// association list just like 'rest', except that the key k is mapped
+// to the value v.  Any other key is mapped to the value it had in 'rest'.
+
+class NonEmptyAList<K extends Comparable<K>,V> implements AList<K,V> {
+
+    private K key; // key for the first entry in this AList
+    private V val; // val for the first entry in this AList
+    private AList<K,V> rest; // the other entries in this AList
+
+    NonEmptyAList (K key, V val, AList<K,V> rest) {
+	this.key = key;
+	this.val = val;
+	this.rest = rest;
+    }
+
+    // Returns an association list like this one
+    // except the given key is associated with the given value.
+
+    // maintains the list in sorted order
+    // use <= so to maintain stable sort for duplicate keys :)
+
+    public AList<K,V> extend (K key, V val) {
+        
+        if (key.compareTo(this.key) <= 0)
+            return new NonEmptyAList<K,V> (key, val, this);
+        else
+            return new NonEmptyAList<K,V>
+                (this.key,
+                 this.val,
+                 this.rest.extend(key,val));
+    }
+
+    // Returns true iff the key is found within this AList.
+
+    public boolean contains (K key) {
+	if (key.equals (this.key))
+	    return true;
+	else
+	    return rest.contains (key);
+    }
+    // Returns the value associated with the given key.
+    // Throws a NoSuchElementException if the key is not found.
+
+    public V lookup (K key) {
+	if (key.equals (this.key))
+	    return this.val;
+	else
+	    return rest.lookup (key);
+    }
+
+    // We will not have reason to compare ALists to each other, so we
+    // won't stop to override equals, toString, or hashcode.
+
+}
---
+++ Examples/10-5-Alist-v6-stateful/AList.java
@@ -0,0 +1,46 @@
+// An AList<K,V> is an object of any class that implements
+// the AList<K.V> interface.
+
+// interpretation: An Alist<K,V> represents a finite partial map from K to V.
+
+// Specification:
+
+// *** NOTICE THAT ALL THESE EQUALITIES ARE OF VALUES AND BOOLEANS,
+// *** NOT OF ALISTS THEMSELVES.  THEY TALK ABOUT THE **BEHAVIOR** OF
+// *** ALISTS. 
+
+// Let a be an alist, k1 and k2 be unequal keys.  Then
+// a.extend(k1,v1).lookup(k1) = v1
+// a.extend(k1,v1).lookup(k2) = a.lookup(k2)
+
+// a.extend(k1,v1).contains(k1) = true
+// a.extend(k1,v1).contains(k2) = a.contains(k2)
+
+// If empty_alist is a representation of the empty alist, then
+// empty_alist.contains(k1) = false
+
+// Note that this is what the English description says.  Furthermore
+// the "=" means the Java equality predicate on the appropriate type.
+// So, for example, the first equation above means
+
+// a.extend(k1,v1).lookup(k1).equals(v1) is true
+
+// Note also that the interface does NOT include a constructor for the
+// empty AList. This is because different implementations of AList may
+// require different arguments to this constructor.
+    
+interface AList<K extends Comparable<K>,V> {
+
+    // extends this AList by associating the given key with the given value.
+
+    void extend (K key, V val);
+
+    // Returns true iff the key is found within this AList.
+
+    boolean contains (K key);
+
+    // Returns the value associated with the given key.
+    // Throws a NoSuchElementException if the key is not found.
+
+    V lookup (K key);
+}
